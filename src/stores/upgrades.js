3d55f6c59365aff22cf7cd38335f92430f65db40
MangoSama1000 <tilija_nitej@hotmail.com>
2024-09-22 13:59:20 +0100

    upgrdae

---
+++ src/stores/upgrades.js
@@ -0,0 +1,25 @@
+const upgrades = [
+    {
+      cost: 10,
+      increment: 1,
+    },
+    {
+      cost: 100,
+      increment: 5,
+    },
+    {
+      cost: 1000,
+      increment: 10,
+    },
+    {
+      cost: 10000,
+      increment: 100,
+    },
+    {
+      cost: 20000,
+      increment: 200,
+    },
+  ];
+  
+  export default upgrades;
+  

7d53204cf0fa307f086be014af41318f22d5b6d9
MangoSama1000 <tilija_nitej@hotmail.com>
2024-09-22 13:54:06 +0100

    header

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Gallywix&apos;s Bank Heist</h1>
+            <p>Click to Steal Gold Coins</p>
+        </header>
+    );
+};
+
+export default Header;
